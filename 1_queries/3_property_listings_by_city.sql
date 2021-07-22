2924b5f6c17d750f0a6be428c9f9bf45a845feeb
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-07-22 05:21:34 +0000

    added seeds/02_seeds for many more table entries to query against. Created 5 queries in 1_queries

---
+++ 1_queries/3_property_listings_by_city.sql
@@ -0,0 +1,8 @@
+SELECT *, AVG(rating) AS average_rating
+FROM properties
+JOIN property_reviews ON property_id = properties.id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id, property_reviews.id
+HAVING AVG(rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
